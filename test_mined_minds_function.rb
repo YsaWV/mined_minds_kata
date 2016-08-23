b4df6c4cca836979fe92b1431ba532df3cc15a0b
Ysabel Bombardiere <ysabelestradab@gmail.com>
2016-08-23 15:07:54 -0400

    updated at 3:08

--- test_mined_minds_function.rb
+++ test_mined_minds_function.rb
@@ -1,9 +1,11 @@
 require "minitest/autorun"
 
-def mined_minds(number)
-	1
+# def mined_minds(number)
+# 	1
 
-end
+# end
+
+require_relative "mined_minds_function.rb"
 
 class TestMinedMindsFunction < Minitest::Test 
 	def test_1_equals_1
@@ -13,5 +15,14 @@ class TestMinedMindsFunction < Minitest::Test
 	def test_1_returns_1
 		assert_equal(1, mined_minds(1))
 	end
+
+	def test_2_returns_2
+		assert_equal(2, mined_minds(2))
+	end
+
+	def test_7_returns_2
+		assert_equal(7, mined_minds(7))
+	end
+
 
 end
